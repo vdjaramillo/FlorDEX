1fae55f835446b4f0591b1e9227f833f2c68e628
David Esteban Escobar Castro <deescobarc@unal.edu.co>
2019-07-02 03:16:55 -0500

    Cambis a nivel visual

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,72 +1,135 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <link rel="stylesheet" href="css/login.css"/>
-        <title>Iniciar Sesión | FlorDEX</title>
-    </head>
-     
-    <body>
-            <section id="background" style="position:absolute; left:0; top:0; width:100%; height:100%">
-                    <?php
-                    $r=rand(1,4); 
-                    echo '<img class="img-fluid" style="width:100%; height:99%" src="img/c'.$r.'.webp" />';
-                ?> 
-            </section>   
-        <div id="lmain"> 
-            <br>
-            <div class="card">
-                <div class="card-header"><h1>{{ __('Login') }}</h1></div>
+@extends('layouts.app')
+@section('title')
+    Iniciar Sesión | FlorDEX
+@endsection
+@section('content')
+            <div id="lmain">
                 <br>
-                    <div class="card-body">
-                        <form method="POST" action="{{ route('login') }}">
-                            @csrf
-                            <div>
-                                <label for="cedula" >Cédula</label>
+                <div class="card">
+                    <div class="card-header"><h1>{{ __('Login') }}</h1></div>
+                    <br>
+                        <div class="card-body">
+                            <form method="POST" action="{{ route('login') }}">
+                                @csrf
                                 <div>
-                                    <input id="cedula" type="number" class="form-control @error('cedula') is-invalid @enderror" name="cedula" value="{{ old('cedula') }}" required autofocus>
-                                        @error('cedula')
-                                            <br>
-                                            <span class="invalid-feedback" role="alert">
-                                                <strong>{{ $message }}</strong>
-                                            </span>
-                                        @enderror
-                                    </div>
-                                </div>
-                                <div>
-                                    <label for="password">{{ __('Password') }}</label>
-                                    <div >
-                                        <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
-        
-                                        @error('password')
-                                            <span class="invalid-feedback" role="alert">
-                                                <strong>{{ $message }}</strong>
-                                            </span>
-                                        @enderror
-                                    </div>
-                                </div>
-                                <div>
-                                    <div >
-                                        <div class="form-check">
-                                            <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
-                                            <label class="form-check-label" id="forgt" for="remember">
-                                                {{ __('Remember Me') }}
-                                            </label>
+                                    <label for="cedula" >Cédula</label>
+                                    <div>
+                                        <input id="cedula" type="number" class="form-control @error('cedula') is-invalid @enderror" name="cedula" value="{{ old('cedula') }}" required autofocus>
+                                            @error('cedula')
+                                                <br>
+                                                <span class="invalid-feedback" role="alert">
+                                                    <strong>{{ $message }}</strong>
+                                                </span>
+                                            @enderror
                                         </div>
                                     </div>
-                                </div>
-                                <div>
-                                    <br>
                                     <div>
-                                        <button type="submit" class="btn btn-primary">
-                                            {{ __('Login') }}
-                                        </button>
+                                        <label for="password">{{ __('Password') }}</label>
+                                        <div >
+                                            <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
+
+                                            @error('password')
+                                                <span class="invalid-feedback" role="alert">
+                                                    <strong>{{ $message }}</strong>
+                                                </span>
+                                            @enderror
+                                        </div>
+                                    </div>
+                                    <div>
+                                        <div >
+                                            <div class="form-check">
+                                                <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
+                                                <label class="form-check-label" id="forgt" for="remember">
+                                                    {{ __('Remember Me') }}
+                                                </label>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div>
                                         <br>
-                                        <br>
+                                        <div>
+                                            <button type="submit" class="btn btn-primary">
+                                                {{ __('Login') }}
+                                            </button>
+                                            <br>
+                                            <br>
+                                </div>
                             </div>
-                        </div>
-                    </form>
+                        </form>
+                    </div>
                 </div>
             </div>
-        </div>
-    </body>
-</html>
+@endsection
+
+{{--<!DOCTYPE html>--}}
+{{--<html>--}}
+{{--    <head>--}}
+{{--        <link rel="stylesheet" href="css/login.css"/>--}}
+{{--        <title>Iniciar Sesión | FlorDEX</title>--}}
+{{--    </head>--}}
+{{--     --}}
+{{--    <body>--}}
+{{--            <section id="background" style="position:absolute; left:0; top:0; width:100%; height:100%">--}}
+{{--                    <?php--}}
+{{--                    $r=rand(1,4); --}}
+{{--                    echo '<img class="img-fluid" style="width:100%; height:99%" src="img/c'.$r.'.webp" />';--}}
+{{--                ?> --}}
+{{--            </section>   --}}
+{{--        <div id="lmain"> --}}
+{{--            <br>--}}
+{{--            <div class="card">--}}
+{{--                <div class="card-header"><h1>{{ __('Login') }}</h1></div>--}}
+{{--                <br>--}}
+{{--                    <div class="card-body">--}}
+{{--                        <form method="POST" action="{{ route('login') }}">--}}
+{{--                            @csrf--}}
+{{--                            <div>--}}
+{{--                                <label for="cedula" >Cédula</label>--}}
+{{--                                <div>--}}
+{{--                                    <input id="cedula" type="number" class="form-control @error('cedula') is-invalid @enderror" name="cedula" value="{{ old('cedula') }}" required autofocus>--}}
+{{--                                        @error('cedula')--}}
+{{--                                            <br>--}}
+{{--                                            <span class="invalid-feedback" role="alert">--}}
+{{--                                                <strong>{{ $message }}</strong>--}}
+{{--                                            </span>--}}
+{{--                                        @enderror--}}
+{{--                                    </div>--}}
+{{--                                </div>--}}
+{{--                                <div>--}}
+{{--                                    <label for="password">{{ __('Password') }}</label>--}}
+{{--                                    <div >--}}
+{{--                                        <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">--}}
+{{--        --}}
+{{--                                        @error('password')--}}
+{{--                                            <span class="invalid-feedback" role="alert">--}}
+{{--                                                <strong>{{ $message }}</strong>--}}
+{{--                                            </span>--}}
+{{--                                        @enderror--}}
+{{--                                    </div>--}}
+{{--                                </div>--}}
+{{--                                <div>--}}
+{{--                                    <div >--}}
+{{--                                        <div class="form-check">--}}
+{{--                                            <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>--}}
+{{--                                            <label class="form-check-label" id="forgt" for="remember">--}}
+{{--                                                {{ __('Remember Me') }}--}}
+{{--                                            </label>--}}
+{{--                                        </div>--}}
+{{--                                    </div>--}}
+{{--                                </div>--}}
+{{--                                <div>--}}
+{{--                                    <br>--}}
+{{--                                    <div>--}}
+{{--                                        <button type="submit" class="btn btn-primary">--}}
+{{--                                            {{ __('Login') }}--}}
+{{--                                        </button>--}}
+{{--                                        <br>--}}
+{{--                                        <br>--}}
+{{--                            </div>--}}
+{{--                        </div>--}}
+{{--                    </form>--}}
+{{--                </div>--}}
+{{--            </div>--}}
+{{--        </div>--}}
+{{--    </body>--}}
+{{--</html>--}}
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,20 +1,40 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <link rel="stylesheet" href="css/index.css"/>
-        <title>FlorDEX |Flores de Altagracia</title>
-    </head>
-    <body>
+@extends('layouts.app')
+@section('title')
+    FlorDEX |Flores de Altagracia
+@endsection
 
-        <section id="main">
-            <nav><ul>
-            <li><a href="{{route('login')}}">Iniciar Sesión</a></li>
-                <li>¿Quiénes somos?</li>
-                <li>Contacto</li>
-            </ul></nav>
-            <h1>FlorDEX</h1>
-            <img src="img/logo.png" alt="Logo Flores de Altagracia">
-            <div>Situada en el valle de Rionegro, cerca de Medellín Colombia, a una altitud de 7.200 pies sobre el nivel del mar y a una temperatura de alrededor de 18 ° C, FLORES DE ALTAGRACIA es uno de los mayores productores de Calla (Zantedeschia sp.) en Colombia con casi 16 acres.</div>
-        </section>
-    </body>
-</html>
+@section('content')
+
+        <div class="container masthead">
+            <div class="intro-text">
+                <span class="name">Flor DEX</span>
+                <hr class="star-light">
+            </div>
+            <img class="img-fluid" src="img/logo1.png" alt="">
+            <div class="intro-text">
+                <span class="skills">Situada en el valle de Rionegro, cerca de Medellín Colombia, a una altitud de 7.200 pies sobre el nivel del mar y a una temperatura de alrededor de 18 ° C, FLORES DE ALTAGRACIA es uno de los mayores productores de Calla (Zantedeschia sp.) en Colombia con casi 16 acres.</span>
+            </div>
+        </div>
+
+@endsection
+
+{{--<!DOCTYPE html>--}}
+{{--<html>--}}
+{{--    <head>--}}
+{{--        <link rel="stylesheet" href="css/index.css"/>--}}
+{{--        <title>FlorDEX |Flores de Altagracia</title>--}}
+{{--    </head>--}}
+{{--    <body>--}}
+
+{{--        <section id="main">--}}
+{{--            <nav><ul>--}}
+{{--            <li><a href="{{route('login')}}">Iniciar Sesión</a></li>--}}
+{{--                <li>¿Quiénes somos?</li>--}}
+{{--                <li>Contacto</li>--}}
+{{--            </ul></nav>--}}
+{{--            <h1>FlorDEX</h1>--}}
+{{--            <img src="img/logo.png" alt="Logo Flores de Altagracia">--}}
+{{--            <div>Situada en el valle de Rionegro, cerca de Medellín Colombia, a una altitud de 7.200 pies sobre el nivel del mar y a una temperatura de alrededor de 18 ° C, FLORES DE ALTAGRACIA es uno de los mayores productores de Calla (Zantedeschia sp.) en Colombia con casi 16 acres.</div>--}}
+{{--        </section>--}}
+{{--    </body>--}}
+{{--</html>--}}
