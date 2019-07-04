58e488b600a801c8fda648072200f1a5fcdcc4b6
vdjaramillo <33434357+vdjaramillo@users.noreply.github.com>
2019-07-04 06:35:15 -0500

    Update admin.blade.php

--- resources/views/authenticated/usuarios/admin.blade.php
+++ resources/views/authenticated/usuarios/admin.blade.php
@@ -4,23 +4,26 @@
 @endsection
 @section('navig')
 <ul class="list-group">
-    @section('links')
+   @section('links')
     <nav>
     <ul>
         <li id="usbtn" onclick="show('usopt')" class="list-group-item bg-soft">Usuarios</li>
-        <a class="usopt list-group-item bg-soft" style="display:none" href="{{route('register')}}"><li>Registrar usuario</li></a>
-        <a class="usopt list-group-item bg-soft" style="display:none" href="{{route('lista-usuarios')}}"><li >Lista de usuarios</li></a>
+            <a class="usopt list-group-item bg-soft" style="display:none; transition: all 1s ease 0s; width:90%" href="{{route('register')}}"><li>Registrar usuario</li></a>
+            <a class="usopt list-group-item bg-soft" style="display:none; transition: all 1s ease 0s; width:90%" href="{{route('lista-usuarios')}}"><li >Lista de usuarios</li></a>
         <li id="usbtn" onclick="show('usoptin')" class="list-group-item bg-soft">Informes</li>
-        <a class="usoptin list-group-item bg-soft" style="display:none" href="{{route('tipo_informe_create')}}"><li >Crear Tipo de Informe</li></a>
-        <a class="usoptin list-group-item bg-soft" style="display:none" href="{{route('tipos_informes_lista')}}"><li >Lista de Informes</li></a>
+        <a class="usoptin list-group-item bg-soft" style="display:none; transition: all 1s ease 0s; width:90%" href="{{route('tipo_informe_create')}}"><li >Crear Tipo de Informe</li></a>
+        <a class="usoptin list-group-item bg-soft" style="display:none; transition: all 1s ease 0s; width:90%" href="{{route('tipos_informes_lista')}}"><li >Lista de Informes</li></a>
     </ul>
     </nav>
     <script>
     function show(clase){
         if($('.'+clase).css('display') == 'none'){
-            $('.'+clase).css({"display":"block"});
+            $('.'+clase).css({"display":"block",
+                              "heigth":"auto",
+                              "margin-left":"8%"});
         }else{
-            $('.'+clase).css({"display":"none"});
+            $('.'+clase).css({"display":"none",
+                              "transition":"all 1s ease 0s"});
         }
     }
       </script>
